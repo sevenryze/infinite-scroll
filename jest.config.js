f289239e671ac7cbedde3804763f62620a28952d
sevenryze <sevenryze@gmail.com>
2018-10-22 23:06:48 +0800

    update

--- jest.config.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  transform: {
-    "^.+\\.(t|j)sx?$": "ts-jest"
-  },
-  testEnvironment: "node",
-  testRegex: `^.*\/test\/(?!.*\.d\.tsx?$).*\.tsx?$`,
-  moduleFileExtensions: ["ts", "tsx", "js", "jsx", "json", "node"]
-};
--- test/easing.test.ts
+++
@@ -1,5 +0,0 @@
-import { easing } from "../lib/easing";
-
-test("easing function should return a number", () => {
-  expect(easing(100, 400)).toBeGreaterThanOrEqual(0);
-});
